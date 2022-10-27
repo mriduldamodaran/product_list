a718a76fbced7642b81a00de7ce8eb2b824c0911
Mridul Damodaran <mriduldamodaran9@gmail.com>
2022-10-27 11:02:33 +0530

    deploy my react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ItemList from "./components/itemList";
+
+function App() {
+  return (
+    <div>
+      
+      <ItemList />
+    </div>
+  );
+}
+
+export default App;
